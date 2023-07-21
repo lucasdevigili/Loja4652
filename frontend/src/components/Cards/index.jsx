5e31d129b4ad55e63254ee4041101029bd34c5bc
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2023-07-21 16:39:06 -0300

    Home Page beta complete

---
+++ frontend/src/components/Cards/index.jsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import colors from '../../themes/colors';
+
+export const Cards = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&family=Source+Sans+Pro&display=swap');
+    height: 480px;
+    width: 310px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    border-radius: 40px;
+    box-shadow: 0px 0px 10px 8px rgba(0, 0, 0, 0.25);
+
+    #cardText {
+        height: 30%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    #cardImg, #cardPrice, #cardButton {
+        display: flex;
+        justify-content: center;
+    }
+
+    #price, .name {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .oldPrice {
+        font-family: 'Source Sans Pro', sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: ${colors.lightGrey};
+    }
+
+
+    .price {
+        font-family: 'Source Sans Pro', sans-serif;
+        font-weight: 700;
+        font-size: 26px;
+
+    }
+
+    #price, .oldPrice, p{
+        width: 70%;
+    }
+
+    #name {
+        width: 60%;
+    }
+
+    .name {
+        font-family: 'Rubik', sans-serif;
+        font-size: 20px;
+        width: 100%;
+    }
+
+    .cardButton {
+        height: 6vh;
+        width: 10vw;
+        border-radius: 50px;
+        border: transparent;
+        font-size: 20px;
+        background-color: ${colors.yellow};
+    }
+`
--- frontend/src/pages/Homepage/Index.js
+++ frontend/src/pages/Homepage/Index.js
@@ -2,29 +2,77 @@ import React from "react";
 import './index.css';
 import { Navbar } from "../../components/Navbar/index";
 import { Carousel } from "../../components/Carousel/index";
-//import { Cards } from "../../components/Cards";
-//import { Contact} from "../../components/Contact";
+import { Card } from "../Catalog/index";
+import { Contact } from "../../components/Contact/Index";
 
-export function Homepage(props) {
+const LIMIT = 12;
+
+export function Homepage() {
     return (
         <>
             <Navbar>
-                    <div id="logo">
-                        <h1>Distrito 4652</h1>
-                    </div>
-                    <div id="nav">
-                        <ul>
-                            <li className="dash">Home</li>
-                            <li className="dash">Catálogo</li>
-                            <li>Contato</li>
-                        </ul>
-                    </div>
-                    <div id="buttons">
-                        <button>Entrar</button>
-                        <button>Cadastrar</button>
-                    </div>
+                <div id="logo">
+                    <h1>Distrito 4652</h1>
+                </div>
+                <div id="nav">
+                    <ul>
+                        <li className="dash">
+                            <button>Home</button></li>
+                        <li className="dash"><button>Catálogo</button></li>
+                        <li><button>Contato</button></li>
+                    </ul>
+                </div>
+                <div id="buttons">
+                    <button>Entrar</button>
+                    <button>Cadastrar</button>
+                </div>
             </Navbar>
-            <Carousel />
+            <Carousel>
+                <div id="carrousel">
+                    <div id="carrouselImg">
+                        <img src="http://via.placeholder.com/1300x460" alt="" className="imgCarrousel" />
+                    </div>
+                </div>
+            </Carousel>
+            <section id="catalog">
+                <div id="cardContainer">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </section>
+            <Contact>
+                <div id="contactContainer">
+                    <div id="contactTitle">
+                        <h3>Informações de contato:</h3>
+                    </div>
+                    <div id="text">
+                        <div id="p">
+                            <p id="email">E-mail:</p>
+                            <p id="instagram">Instagram:</p>
+                            <p id="cellphone">Telefone:</p>
+                        </div>
+                        <div id="span">
+                            <span className="email">distrito4652@gmail.com</span>
+                            <span className="instagram">@interact4652</span>
+                            <span className="cellphone">(47)9262-8160</span>
+                        </div>
+                    </div>
+                </div>
+            </Contact>
         </>
     );
 }
