8e9b913802f5d5d2e9811e423f8419acb94a221d
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2023-08-17 19:50:01 -0300

    instalando recursos emodificando o front

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
+import productsRoutes from "./routes/products.js";
 import cors from "cors";
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/", userRoutes)
+app.use("/prduct", productsRoutes)
 
 app.listen(8800)
